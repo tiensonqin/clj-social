e526f9a9b6655fb5a9d03f268071b250679210f7
Tienson Qin <tiensonqin@gmail.com>
2020-07-07 17:19:30 +0800

    Fix dropbox link

--- src/clj_social/dropbox.clj
+++ src/clj_social/dropbox.clj
@@ -1,6 +1,6 @@
 (ns clj-social.dropbox
   (:import [com.github.scribejava.apis DropboxApi]))
 
-(def spec {:user-url "https://api.dropboxapi.com/2/users/get_space_usage"
+(def spec {:user-url "https://api.dropboxapi.com/2/users/get_current_account"
            :api (DropboxApi/instance)
            :params {"access_token" :access-token}})
